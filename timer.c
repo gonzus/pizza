a1141743f7dd2ff3815bcd9929378cf7ce3a4597
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2020-06-05 14:04:24 +0200

    Use some constants to DRY

--- timer.c
+++ timer.c
@@ -2,6 +2,9 @@
 #include <time.h>
 #include "timer.h"
 
+#define USECS_IN_A_MSEC  1000UL
+#define USECS_IN_A_SEC   (USECS_IN_A_MSEC * USECS_IN_A_MSEC)
+
 void timer_start(Timer* t) {
     gettimeofday(&t->t0, NULL);
 }
@@ -10,24 +13,22 @@ void timer_stop(Timer* t) {
     gettimeofday(&t->t1, NULL);
 }
 
-unsigned long timer_elapsed_us(Timer* t)
-{
-    return ((t->t1.tv_sec  - t->t0.tv_sec ) * 1000000UL +
+unsigned long timer_elapsed_us(Timer* t) {
+    return ((t->t1.tv_sec  - t->t0.tv_sec ) * USECS_IN_A_SEC +
             (t->t1.tv_usec - t->t0.tv_usec));
 }
 
-void timer_format_elapsed(Timer* t, FILE* fp, int newline)
-{
+void timer_format_elapsed(Timer* t, FILE* fp, int newline) {
     static struct {
         unsigned long min;
         const char* name;
     } units[] = {
-        { 24UL * 60UL * 60UL * 1000000UL, "D"  },
-        {        60UL * 60UL * 1000000UL, "h"  },
-        {               60UL * 1000000UL, "m"  },
-        {                      1000000UL, "s"  },
-        {                         1000UL, "ms" },
-        {                            0UL, "us" },  // default, catch all
+        { 24UL * 60UL * 60UL * USECS_IN_A_SEC, "D"  },
+        {        60UL * 60UL * USECS_IN_A_SEC, "h"  },
+        {               60UL * USECS_IN_A_SEC, "m"  },
+        {                      USECS_IN_A_SEC, "s"  },
+        {                     USECS_IN_A_MSEC, "ms" },
+        { /* default, catch all */        0UL, "us" },
     };
     unsigned long us = timer_elapsed_us(t);
     for (int j = 0; 1; ++j) {

d38b511da83e7c4b4026ba7d4e1b6fc812d4e6c8
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-09-24 12:21:28 +0200

    Timer: use clock_gettime instead of gettimeofday

--- timer.c
+++ timer.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
-#include <time.h>
 #include "util.h"
 #include "timer.h"
 
 void timer_start(Timer* t) {
-    gettimeofday(&t->t0, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &t->ts0);
 }
 
 void timer_stop(Timer* t) {
-    gettimeofday(&t->t1, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &t->ts1);
+}
+
+unsigned long timer_elapsed_ns(Timer* t) {
+    return ((t->ts1.tv_sec  - t->ts0.tv_sec ) * NSECS_IN_A_SEC +
+            (t->ts1.tv_nsec - t->ts0.tv_nsec));
 }
 
 unsigned long timer_elapsed_us(Timer* t) {
-    return ((t->t1.tv_sec  - t->t0.tv_sec ) * USECS_IN_A_SEC +
-            (t->t1.tv_usec - t->t0.tv_usec));
+    return timer_elapsed_ns(t) / NSECS_IN_A_USEC;
+}
+
+unsigned long timer_elapsed_ms(Timer* t) {
+    return timer_elapsed_us(t) / USECS_IN_A_MSEC;
+}
+
+unsigned long timer_elapsed_s(Timer* t) {
+    return timer_elapsed_ms(t) / MSECS_IN_A_SEC;
 }
 
 void timer_format_elapsed(Timer* t, struct Buffer* b) {

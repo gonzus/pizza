eec61cce8e527cbb32e17cd9b64068f703aa8dfa
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-09-27 16:49:31 +0200

    util: cleanup and improve

--- util.h
+++ util.h
@@ -5,18 +5,25 @@
  * Utility functions.
  */
 
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 // We just use a pointer
 struct Buffer;
 
-#include <stdio.h>
+// Dump a block of bytes into a FILE stream with nice formatting.
+void dump_bytes(FILE* fp, const void* ptr, size_t len);
 
-// dump a range of bytes into fp, in a readable way
-void dump_bytes(FILE* fp, const unsigned char* bdat, int blen);
-
-// format a number of microseconds into a Buffer in a detailed manner
+// Format a number of microseconds (us) into a Buffer, in detailed or
+// abbreviated form.
 void format_detailed_us(unsigned long us, struct Buffer* b);
-
-// format a number of microseconds into a Buffer in an abbreviated manner
 void format_abbreviated_us(unsigned long us, struct Buffer* b);
+
+// Realloc a chunk of memory to a given size.  Useful to:
+// len == 0            free memory pointed to by ptr
+// len > 0, ptr == 0   allocate a new chunk of memory of len bytes
+// len > 0, ptr != 0   reallocate a chunk of memory of len bytes
+void* memory_realloc(void* ptr, size_t len);
 
 #endif

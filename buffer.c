2f4ea3b6f813871c754ed2f811af54d2a2a3aa79
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2021-11-01 13:07:19 +0100

    Improve buffer_append_file()

--- buffer.c
+++ buffer.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include "memory.h"
@@ -10,6 +11,8 @@
 
 #define BUFFER_DEFAULT_CAPACITY BUFFER_DESIRED_SIZE  // default size for Buffer
 #define BUFFER_GROWTH_FACTOR                      2  // how Buffer grows when needed
+
+#define BUFFER_SIZE 4096
 
 static void buffer_adjust(Buffer* b, uint32_t cap);
 static void buffer_append_ptr_len(Buffer* b, const char* ptr, int len);
@@ -126,25 +129,29 @@ void buffer_append_buffer(Buffer* b, const Buffer* buf) {
     buffer_append_ptr_len(b, buf->ptr, buf->len);
 }
 
-void buffer_append_file(Buffer* b, const char* name) {
+int buffer_append_file(Buffer* b, const char* name) {
+    int ret = 0;
     FILE* fp = 0;
     do {
         fp = fopen(name, "r");
         if (!fp) {
+            ret = errno;
             break;
         }
 
-        char tmp[1024];
-        size_t len = fread(tmp, 1, 1024, fp);
-        if (!len) {
+        char tmp[BUFFER_SIZE];
+        size_t nread = fread(tmp, 1, BUFFER_SIZE, fp);
+        if (!nread) {
             break;
         }
 
-        buffer_append_string(b, tmp, len);
+        buffer_append_string(b, tmp, nread);
     } while (0);
     if (fp) {
         fclose(fp);
+        fp = 0;
     }
+    return ret;
 }
 
 void buffer_format_signed(Buffer* b, long long l) {
